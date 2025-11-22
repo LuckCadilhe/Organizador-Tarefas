66b53378b202ff36f0456a4b952a18ecdb3c632b
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2025-11-22 00:47:48 -0300

    Feito processo de adicionar tarefa e checagem das mesmas com opção de menu

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
-class tarefas():
+
+organizador_tarefas = {
+    1: 'Adicionar nova tarefa',
+    2: 'Listar tarefas existentes',
+    3: 'Marcar tarefa como concluída',
+    4: 'Editar tarefa',
+    5: 'Remover tarefa',
+    6: 'Sair do sistema'
+}
+
+def exibir_menu():
+        print('\n --- Menu Organizador Tarefas ---')
+        for chave, valor in organizador_tarefas.items():
+            print(f'{chave} - {valor}')
+        print('----------------------------')
+
+class GerenciadorTarefas:
     def __init__(self):
         self.tarefas = []
         
-    def adicionar_tarefas(self, tarefa):
-        self.tarefas.append(tarefa)
-        print(f'Tarefa {tarefa} adicionada.')
-    
+    def adicionar_tarefas(self):
+        
+        while True:
+            saida = input('\nDeseja adicionar uma nova tarefa? (s/n): ').lower().strip()
+            
+            if saida == 'n':
+                print('\nVocê saiu das tarefas.')
+                break
+            
+            if saida != 's':
+                print('\nErro, opção invalida. Tente novamente.')
+                continue
+                
+            nova_tarefa = input('Digite a sua tarefa: ')
+        
+            if nova_tarefa.strip() == '':
+                print('\nErro na tarefa (entrada vazia)')
+            else:
+                self.tarefas.append(nova_tarefa.strip())
+                print(f'\nTarefa {nova_tarefa.strip()} adicionada.')
+                  
     def checar_tarefas(self):
+        
         if not self.tarefas:
             print('Não há tarefas listadas.')
         else:
             print('\n--- Lista de Tarefas ---')
-            for index, tarefas in enumerate(self.tarefas, start=1):
-                print(f'Tarefa  {index}: {tarefas}')
+            for index, tarefa in enumerate(self.tarefas, start=1):
+                print(f'Tarefa {index}: {tarefa}')
             print('-------------------------')
+
         
-lista = tarefas()
+gerenciador = GerenciadorTarefas() 
 
-while True:
-    entrada = input('Adicione uma nova tarefa ou\ndigite "sair" para terminar a operação: ')
-
-    if entrada.lower() == 'sair':
-        print('Encerrar processo de adição de tarefas.')
-        break
-    
-    if entrada:
-        lista.adicionar_tarefas(entrada)
-
-lista.checar_tarefas()           
-
+def menu_principal():
+    while True:
+        exibir_menu()
+        
+        try:
+            opcao = int(input('Digite a opção desejada de 1 a 6 : '))
+        except ValueError:
+            print('Número errado, escolha novamente.')
+            continue
+        
+        if opcao == 1:
+            gerenciador.adicionar_tarefas()
+        elif opcao == 2:
+            gerenciador.checar_tarefas()
+            input('\nPressione uma ENTER para voltar ao Menu.')
+            continue
+        elif opcao == 6:
+            break
+        elif opcao in organizador_tarefas:
+            print(f"A opção '{organizador_tarefas[opcao]}' ainda não foi implementada.")
+        else:
+            print("Opção inválida. Tente novamente.")
+            
+if __name__ == "__main__":
+    menu_principal()
